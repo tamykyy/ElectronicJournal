1aeda8451559efb5ec06bc4750c4d61812488985
Mykyta Vykhodtsev <deriel20021903@gmail.com>
2022-06-02 20:07:12 +0300

    create spring boot application

---
+++ src/test/java/edu/team/electronic_journal/ElectronicJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.team.electronic_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
